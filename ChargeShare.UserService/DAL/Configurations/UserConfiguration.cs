99087feb7996d6c5fe2d5502f19feaf728a7ebd2
JustFalco <falco@wolkorte.nl>
2022-12-13 09:15:55 +0100

    started with basic controller and DAL

---
+++ ChargeShare.UserService/DAL/Configurations/UserConfiguration.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace ChargeShare.UserService.DAL.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
